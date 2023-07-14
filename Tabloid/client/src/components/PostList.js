ac2dfc99ff65e80397da3ba84c0c5c846c02a781
Adam Riggs <adambolan31@gmail.com>
2023-07-14 15:06:51 -0500

    added viewed posts

---
+++ Tabloid/client/src/components/PostList.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import { Post } from "./Post";
+
+export const PostList = () => {
+    const baseUrl = '/api/Post/'
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch(baseUrl).then(res => (res.json())).then(posts => setPosts(posts))
+
+    }
+        , []
+    )
+
+    return (
+        <div>
+            {posts.map((post) => (<Post post={post} key={post.id} />))}
+        </div>
+    )
+}
+
+export default PostList
