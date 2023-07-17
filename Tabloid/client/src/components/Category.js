1545f85dc626e95c5715e31a417feb1671e62711
Nic <nic.lahde@gmail.com>
2023-07-17 14:45:59 -0500

    add category edit

--- Tabloid/client/src/components/Category.js
+++ Tabloid/client/src/components/Category.js
@@ -1,6 +1,11 @@
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const Category = ({ category, setCategories }) => {
+    const [categoryToEdit, setCategoryToEdit] = useState({
+        id: category.id,
+        name: ""
+    })
+    const [editMode, setEditMode] = useState(false)
     const baseUrl = '/api/Category/'
     const handleDelete = (e) => {
         e.preventDefault();
@@ -13,9 +18,55 @@ export const Category = ({ category, setCategories }) => {
                 })
         }
     }
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setCategoryToEdit((category) => ({
+            ...category,
+            [name]: value
+        }));
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch(baseUrl, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(categoryToEdit)
+        })
+            .then(res => res.json())
+            .then(response => {
+                setCategories(response);
+                setEditMode(false);
+            })
+            .catch(error => {
+                console.error("Error updating category:", error);
+            });
+    }
     return (
         <div>
-            <p>Name: {category.name}  <button onClick={handleDelete}>Delete</button></p>
+            {editMode ? (
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Edit Category {category.name}:
+                        <input
+                            type="text"
+                            name="name"
+                            value={categoryToEdit.name}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <button type="submit">Submit</button>
+                    <button onClick={() => setEditMode(false)}>Cancel</button>
+                </form>
+            ) : (
+                <p>
+                    Name: {category.name}
+                    <button onClick={() => setEditMode(true)}>Edit</button>
+                    <button onClick={handleDelete}>Delete</button>
+                </p>
+            )}
         </div>
     )
 }
