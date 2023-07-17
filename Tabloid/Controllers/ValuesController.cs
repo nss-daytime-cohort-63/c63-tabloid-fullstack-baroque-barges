b46d60dde9648f0ce066456faef0e644ce24bf65
nicholas pushic <119707869+pistachiosp@users.noreply.github.com>
2023-07-17 10:53:11 -0400

    still working 35

---
+++ Tabloid/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tabloid.Repositories;
+
+namespace Tabloid.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private readonly ITagRepository _tagRepository;
+        public TagController(ITagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetTag()
+        {
+            return Ok(_tagRepository.GetAllTags());
+        }
+
+    }
+}
